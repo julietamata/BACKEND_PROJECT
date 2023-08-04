81e6905010b9c5004f8b0267414682df79e6d1a4
Julieta Mata <julieta.mata.sandoval@gmail.com>
2023-08-03 23:10:33 -0600

    Comentarios para desafíos

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,104 +1,3 @@
-// import { Router } from "express";
-// import userModel from "../dao/models/users.model.js";
-
-// const router = Router()
-
-// // Ruta de errores
-
-// router.get("/error", (req, res) => {
-//   res.render("partials/errors");
-// });
-
-// // Vista de Login
-// router.get("/login", (req, res) => {
-//     res.render("session/login");
-//   });
-
-// // Ruta para hacer login
-// router.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   if (!email || !password)
-//     return res.status(400).json({ status: "error", error: "Favor de llenar todos los campos" });
-
-//   const user = await userModel.findOne({ email: email });
-
-//   if (!user)
-//     return res.status(401).render("partials/errors", { error: "Wrong password or username" });
-//   if (!isValidPassword(user, password))
-//     return res.status(401).render("partials/errors", { error: "Wrong password or username" });
-//   req.session.user = user;
-//   res.redirect("/mongoose/products");
-// });
-
-
-
-// // Vista del Registro de usuario
-//   router.get("/register", (req, res) => {
-//     res.render("session/register");
-//   });
-
-// // Vista para hacer registro
-//   router.post("/register", async (req, res) => {
-//     const { first_name, last_name, email, age, password } = req.body;
-//     if (!first_name || !last_name || !email || !age)
-//       return res.status(400).json({ status: "error", message: "error Register" });
-
-//     let user = {
-//       first_name,
-//       last_name,
-//       email,
-//       age,
-//       password: createHash(password),
-//     };
-
-//     const existUser = await userModel.findOne({ email: user.email });
-
-//     if (existUser)
-//       return res.json({
-//         status: "error",
-//         message: "Ya existe un usuario con ese email",
-//       });
-
-//     await userModel.create(user);
-//     res.redirect("/session/login");
-// });
-
-
-// // Ruta de errores
-
-// router.get("/error", (req, res) => {
-//   res.render("partials/errors");
-// });
-
-
-// // Vista para hacer logout
-//   router.get('/logout', (req, res) => {
-//     req.session.destroy(err => {
-//         if(err) {
-//             console.log(err);
-//             res.status(500).render('partials/error', {error: err})
-//         } else res.redirect('/session/login')
-//     })
-//   })
-  
-
-
-// export default router
-
-
-
- 
-
-
-
-
-
-
-
-
-// session.router.js
-
 import { Router } from "express";
 import usersModel from "../dao/models/users.model.js";
 import bcrypt from "bcrypt";
@@ -108,17 +7,20 @@ import initializePassport from "../config/passport.config.js";
 
 const router = Router();
 
-// contraseñas 
-
-
-
 
 // Vista de Login
 router.get("/login", (req, res) => {
   res.render("session/login");
 });
 
-// // Ruta para hacer login
+
+// Vista del Registro de usuario
+router.get('/register', (req, res) => {
+  res.render('session/register');
+});
+
+
+// // Ruta para hacer login ** Habilitar para desafío de login sin passport**
 // router.post("/login", async (req, res) => {
 //   const { email, password } = req.body;
 
@@ -143,10 +45,6 @@ router.get("/login", (req, res) => {
 //   res.redirect("/mongoose/products");
 // });
 
-// Vista del Registro de usuario
-router.get('/register', (req, res) => {
-  res.render('session/register');
-});
 
 // //Vista para hacer registro
 
@@ -197,18 +95,16 @@ router.get('/register', (req, res) => {
 //   res.redirect("/session/login");
 // });
 
+//** **
 
 
-
-
-// Vista para hacer registro con passport
+// Vista para hacer registro con passport ** Ocultar para desafío de login **
 
 router.post('/register', passport.authenticate('register', {
   failureRedirect: 'session/error'
 }), async (req, res) => {
   res.redirect('/session/login');
 });
-
 
 
 router.post('/login', passport.authenticate('login', {failureRedirect: 'session/error'}), async (req, res) => {
